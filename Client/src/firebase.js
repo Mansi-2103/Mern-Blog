38ce83cf59791d024d710603b26cda9301dfd887
Mansi-2103 <mansikri2103@gmail.com>
2025-07-24 22:26:14 +0530

    Updated something

---
+++ Client/src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID",
+  // ... any other config fields
+};
+
+export const app = initializeApp(firebaseConfig);
